ba72c0defd0ab592f02458df1bd6e7175e93a509
dewstouh <diego@justdiego.com>
2025-07-12 17:56:19 +0200

    chore(web): remove margin top from CTA and add margin bottom

--- apps/web/components/CTA.tsx
+++ apps/web/components/CTA.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function CTA() {
   return (
-    <div className = "text-center mt-16 pt-8 border-t-2 border-gray-300" >
+    <div className = "text-center mb-8 pt-8 border-t-2 border-gray-300" >
       <h3 className="text-2xl font-bold text-gray-900 mb-4">
         Will your business be next?
       </h3>
