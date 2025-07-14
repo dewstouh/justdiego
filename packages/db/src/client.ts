da2b65aa15eb9f3b36f34715cb78380e02f15bde
dewstouh <diego@justdiego.com>
2025-07-14 20:00:55 +0200

    refactor(db): export generated prisma and prisma client in index.ts

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -3,8 +3,6 @@ import { PrismaClient } from "../generated/prisma";
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-    globalForPrisma.prisma || new PrismaClient();
+    globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export * from "../generated/prisma";
