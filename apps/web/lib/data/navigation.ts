50eabc90d916adf70e15bb99d25eb2a4ad533276
dewstouh <diego@justdiego.com>
2025-07-15 21:22:31 +0200

    refactor(web): remove promise for testing getnavigations suspense

--- apps/web/lib/data/navigation.ts
+++ apps/web/lib/data/navigation.ts
@@ -2,7 +2,5 @@
 import db from "@justdiego/db"
 
 export const getNavigations = async () => {
-    // Add delay for testing suspense effect
-    await new Promise(resolve => setTimeout(resolve, 2000));
     return db.navigationItem.findMany();
 }
