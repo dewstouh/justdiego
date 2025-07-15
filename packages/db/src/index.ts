f14d7b5d22a9cea629e03276489c674011a20380
dewstouh <diego@justdiego.com>
2025-07-15 15:47:23 +0200

    refactor(db): remove repository usage and export prisma as db

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,11 +1,2 @@
-import { PrismaClient } from './types'
-import {prisma} from './client'
-import { Repository } from './utils/Repository'
-
-type ModelName = keyof PrismaClient
-
-export const db = Object.fromEntries(
-    Object.entries(prisma)
-        .filter(([key, value]) => typeof value === 'object' && value !== null && 'findMany' in value)
-        .map(([modelName, model]) => [modelName, new Repository(model)])
-) as Record<ModelName, Repository<any>>
+import {prisma as db} from './client'
+export default db;
