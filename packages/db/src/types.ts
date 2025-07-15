7d07669147411804c16157f86a867a8e739ec498
dewstouh <diego@justdiego.com>
2025-07-15 15:46:53 +0200

    refactor(db/types): export prisma generated types only for now

--- packages/db/src/types.ts
+++ packages/db/src/types.ts
@@ -1,3 +1,1 @@
-export type { PrismaClient } from "../generated/prisma" // exports generated types from prisma
-export type * from "../generated/zod" // exports generated types from zod
-export { }
+export * from "../generated/prisma" // exports generated types from prisma
