204a583e4c433e5de370a71561cfc80fb28363ac
dewstouh <diego@justdiego.com>
2025-07-15 23:40:40 +0200

    feat(web): add tools page placeholder

---
+++ apps/web/app/(marketing)/tools/page.tsx
@@ -0,0 +1,11 @@
+import { SimplePage } from "../../../components/Page";
+
+export default function Page() {
+    return (
+        <SimplePage config={{
+            title: "Tools to Level Up Your Business",
+            description: "Find every leak, automate the inefficiency, and fix what’s losing you money.",
+        }}>
+        </SimplePage>
+    )
+}
