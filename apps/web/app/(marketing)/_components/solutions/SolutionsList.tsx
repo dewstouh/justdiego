41782b97437332da148301d7095529ca3fe030f1
dewstouh <diego@justdiego.com>
2025-07-15 18:45:46 +0200

    refactor(web): add dedicated component to list the solutions

---
+++ apps/web/app/(marketing)/_components/solutions/SolutionsList.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import SolutionCard from '../../../../components/SolutionCard';
+import { getSolutions } from '../../../../lib/data/solution';
+
+export default async function SolutionsList() {
+    const solutions = await getSolutions();
+  return (
+      <div className="grid gap-8 lg:gap-12">
+          {solutions.slice(0, 2).map((solution, index) => {
+              return (
+                  <SolutionCard
+                      solution={solution}
+                      key={solution.id}
+                      variant="compact"
+                      showSeparator={index > 0}
+                  />
+              );
+          })}
+      </div>
+  )
+}
